8cc263be0dc467da57c1315e8e414e704d7fd4a3
Juan <juanantonioamillopez@gmail.com>
2021-10-26 16:17:48 +0200

    Formularios para empleados y utiles

--- php/empresas.php
+++ php/empresas.php
@@ -12,22 +12,67 @@ $MyBBDD->consulta($sql);
 
 $fila = $MyBBDD->extraerRegistro();
 echo "<h1>" . $fila['Nombre_Empresa'] . "</h1>";
-echo "<p> TABLA INVENTARIO  <p>";
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/Alma-Zen/css/estilo.css">
     <title>
         <?php
         echo $fila['Nombre_Empresa']; // El nombre de cada empresa aparecerá en la pestaña del navegador
         ?>
     </title>
+
 </head>
+
 <body>
-    
+
+
+    <?php
+    /*
+        PENDIENTE: Aquí irán apareciendo elementos <div> que agruparán los distintos
+        elementos intriducidos por formulario
+    */
+    ?>
+
+
+
+    <div id="formularios">
+        <div>
+            <h3>EMPLEADOS</h3>
+            <form action="">
+                <input type="text" value="">
+                <input type="submit" name="addEmple" value="insertar impleado">
+            </form>
+        </div>
+
+
+        <div>
+            <h3>ÚTILES</h3>
+            <form action="">
+                <input type="text" value="">
+                <input type="submit" name="addEmple" value="insertar impleado">
+            </form>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
 </body>
+
 </html>
