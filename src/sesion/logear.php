e74fa8c005259604b7d354f8e98c829a6037c8d0
ToniMDev <antonio6_mmc@hotmail.com>
2021-12-01 23:21:55 +0100

    nombre

--- src/sesion/logear.php
+++ src/sesion/logear.php
@@ -8,15 +8,24 @@ $contrasena = $_POST['contrasena'];
 
 
 
-$consulta = "SELECT * FROM usuarios WHERE user='$usuario' AND password='$contrasena';";
-echo $consulta;
-$MyBBDD->consulta($consulta);
+$consultaLogin = "SELECT * FROM usuarios WHERE user='$usuario' AND password='$contrasena';";
+
+$MyBBDD->consulta($consultaLogin);
 $fila = $MyBBDD->extraerRegistro(); //nos devuelve los datos de la sentencia
 
 if ($fila == false) { //si el logeo falla
    // echo "Datos incorrectos";
-    header("location: index.php"); //Nos lleva a login
- }  else { // si el logeo es exitoso
-    $_SESSION['id_empresa'] = $fila['id_empresa'];
-    header("location: ../empresas.php");  //nos lleva a la página de empresas
-     }
+   header("location: login.php"); //Nos lleva a login
+} else { // si el logeo es exitoso
+   //CREAMOS LA VARIABLE SESION CON EL NOMBRE DE LA EMPRESA
+   $_SESSION['id_empresa'] = $fila['id_empresa'];
+   $id_empresa = $fila['id_empresa']; //Esta variable es necesaria porque si hago la sentencia con $sesion o $fila me peta por las comillas
+
+   //CREAMOS LA VARIABLE SESION CON EL NOMBRE DE LA EMPRESA
+   $consultaNombreEmpresa = "SELECT nombre_empresa FROM empresas WHERE  id_empresa='$id_empresa';";
+   $MyBBDD->consulta($consultaNombreEmpresa);
+   $fila = $MyBBDD->extraerRegistro(); //nos devuelve los datos de la sentencia
+   $_SESSION['nombre_empresa'] = $fila['nombre_empresa'];
+
+   header("location: ../index.php"); 
+}
