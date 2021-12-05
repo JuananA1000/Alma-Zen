ebae6ccb3a6b14c00cc60fea43cb564ed457b879
Juan <juanantonioamillopez@gmail.com>
2021-12-05 18:52:32 +0100

    Estilamos Login

--- src/sesion/login.php
+++ src/sesion/login.php
@@ -11,7 +11,8 @@ include '../conect_class.php'; // MUY IMPORTANTE.
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+   <link rel="stylesheet" href="src/style.css">
+    <title>Login</title>
 </head>
 
 <body>
